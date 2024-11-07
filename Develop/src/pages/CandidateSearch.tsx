405e229a0c19e00d6a451de4161b0d7ad04c4ae3
Pravus696 <tbell3393@gmail.com>
2024-11-07 15:49:18 -0600

    candidate interface changes made and search code added but incomplete

--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -1,9 +1,41 @@
-import { useState, useEffect } from 'react';
-import { searchGithub, searchGithubUser } from '../api/API';
+import { useState, useEffect } from "react";
+import { searchGithub, searchGithubUser } from "../api/API";
+import { Candidate } from "../types/Candidate";
 
 const CandidateSearch = () => {
+  const [candidate, setCandidates] = useState<Candidate[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  return <h1>CandidateSearch</h1>;
+  useEffect(() => {
+    try {
+      searchGithub().then((data) => {
+        setCandidates(data);
+        setIsLoading(false);
+      });
+    }
+    catch (err) {
+      console.log('an error occurred', err);
+    }
+  }
+);
+
+  if (isLoading) {
+    return (
+      <section>
+        <h1>Candidate Search</h1>
+        <div>
+          <p>Loading...</p>
+        </div>
+      </section>
+    );
+  }
+  return (
+    <section>
+      <h1>Candidate Search</h1>
+        <p>Your Candidates Here</p>
+      </section>
+    
+  );
 };
 
 export default CandidateSearch;
