bcaa861cfcf2ab22b9a226c701905655e07eb720
Pravus696 <tbell3393@gmail.com>
2024-11-07 15:25:03 -0600

    added candidate interface with all possible return objects from API

--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 
 const CandidateSearch = () => {
+
   return <h1>CandidateSearch</h1>;
 };
 
